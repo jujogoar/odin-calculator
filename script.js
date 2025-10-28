a319b171255035ae111161251518753b96026ab6
Jujo <28857218+jujogoar@users.noreply.github.com>
2025-10-28 10:03:41 +0100

    Buttons click event logic

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ let number2 = ""
 let operator = ""
 
 function getOperation() {
-    number1 = document.getElementById("#calculator #screen #number1").innerHTML
+    number1 = document.getElementById("number1").innerHTML
     if (number1 == "") {
         number1 = 0
     }
-    operator = document.getElementById("#calculator #screen #operator").innerHTML
-    number2 = document.getElementById("#calculator #screen #number2").innerHTML
-    if (number2 == "") {
-        number2 = 0
-    }
+    operator = document.getElementById("operator").innerHTML
+    number2 = document.getElementById("number2").innerHTML
 }
 
 getOperation()
+
+function setOperation() {
+    document.getElementById("number1").innerHTML = number1
+    document.getElementById("number2").innerHTML = number2
+    document.getElementById("operator").innerHTML = operator
+}
 
 function add() {
     return number1 + number2
@@ -54,3 +57,24 @@ function operate() {
         return divide()
     }
 }
+
+document.querySelectorAll("#calculator #buttons .line button").forEach((button) => {
+    const key = button.innerHTML
+    button.addEventListener("click", () => {
+        if (key == "C") {
+            number1 = 0
+            number2 = ""
+            operator = ""
+        } else if (!Number.isInteger(Number.parseInt(key)) && operator == "") {
+            operator = key
+        } else if (Number.isInteger(Number.parseInt(key))) {
+            if (operator == "") {
+                number1 = Number.parseInt(`${number1}${key}`)
+            } else {
+                number2 = Number.parseInt(`${number2}${key}`)
+            }
+        }
+
+        setOperation()
+    }, false);
+});
